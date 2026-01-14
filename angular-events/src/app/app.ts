a1d3c6e137e8618ae023d05e7643d991b12b84bb
Karan Kumawat <karan.kumar@robomq.io>
2026-01-14 16:16:19 +0530

    loops

--- angular-events/src/app/app.ts
+++ angular-events/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { InputComponent } from './input/input.component';
 import { Form } from './form/form';
 import { Condition } from './condition/condition';
+import { Loop } from './loop/loop';
 
 @Component({
   selector: 'app-root',
-  imports: [InputComponent, Form, Condition],
+  imports: [InputComponent, Form, Condition, Loop],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ angular-events/src/app/loop/loop.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loop',
+  imports: [],
+  templateUrl: './loop.html',
+})
+export class Loop {
+  users = [
+    { name: 'Karan Kumawat', email: 'karan.kumawat@example.com' },
+    { name: 'Aarav Sharma', email: 'aarav.sharma@example.com' },
+    { name: 'Neha Verma', email: 'neha.verma@example.com' },
+    { name: 'Rohit Singh', email: 'rohit.singh@example.com' },
+    { name: 'Priya Patel', email: 'priya.patel@example.com' },
+  ];
+}
