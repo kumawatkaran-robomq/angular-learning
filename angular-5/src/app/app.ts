520090eeaf9989c167ad14f44b853304cd1c1af2
Karan Kumawat <karan.kumar@robomq.io>
2026-01-19 18:16:53 +0530

    lifecycle

--- angular-5/src/app/app.ts
+++ angular-5/src/app/app.ts
@@ -1,13 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ParentToChild } from './parent-to-child/parent-to-child';
 import { FormsModule } from '@angular/forms';
 import { ChildToParent } from './child-to-parent/child-to-parent';
 import { BuiltinPipe } from './builtin-pipe/builtin-pipe';
 import { CustomPipe } from './custom-pipe/custom-pipe';
+import { Lifecycle } from './lifecycle/lifecycle';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ParentToChild, ChildToParent, BuiltinPipe, CustomPipe, FormsModule],
+  imports: [ParentToChild, ChildToParent, BuiltinPipe, CustomPipe, FormsModule, Lifecycle, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -15,6 +17,7 @@ export class App {
   users = ['karan', 'tushar', 'piyush', 'ronak'];
   name = 'Karan';
   city = '';
+  count = 0;
   handleCity(value: string) {
     this.city = value;
   }
---
+++ angular-5/src/app/lifecycle/lifecycle.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle',
+  templateUrl: './lifecycle.html',
+})
+export class Lifecycle {
+  constructor() {
+    console.log('Constructor called');
+  }
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+  ngOnDestroy() {
+    console.log('Component destroyed');
+  }
+}
