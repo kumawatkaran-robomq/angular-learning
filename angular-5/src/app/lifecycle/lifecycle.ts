68cebaeb6ae4521f1240dcd0869c614b52da2b6b
Karan Kumawat <karan.kumar@robomq.io>
2026-01-20 10:38:19 +0530

    diff

--- angular-5/src/app/lifecycle/lifecycle.ts
+++ angular-5/src/app/lifecycle/lifecycle.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle',
   templateUrl: './lifecycle.html',
 })
 export class Lifecycle {
+  @Input() name!: string;
   constructor() {
+    console.log('name in constructor', this.name);
     console.log('Constructor called');
   }
   ngOnInit() {
+    console.log('name in ngOnInit', this.name);
     console.log('ngOnInit');
   }
   ngDoCheck() {
@@ -18,3 +21,12 @@ export class Lifecycle {
     console.log('Component destroyed');
   }
 }
+
+// 1. Component ka object banta hai
+//    → constructor()
+
+// 2. Parent se @Input values aati hain
+//    → name = 'Karan'
+
+// 3. Component initialize hota hai
+//    → ngOnInit()
