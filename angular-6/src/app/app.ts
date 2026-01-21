241634ec9d80faec604d6b633ebc8461fb48ae59
Karan Kumawat <karan.kumar@robomq.io>
2026-01-21 12:06:07 +0530

    attr binding

--- angular-6/src/app/app.ts
+++ angular-6/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AttributeBinding } from './attribute-binding/attribute-binding';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AttributeBinding],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-6');
