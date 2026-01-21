3a7c1e5c1634c779c2c970fa44fff1bd4703680f
Karan Kumawat <karan.kumar@robomq.io>
2026-01-21 10:29:26 +0530

    angular-6

---
+++ angular-6/src/app/attribute-binding/attribute-binding.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute-binding',
+  imports: [],
+  templateUrl: './attribute-binding.html',
+})
+export class AttributeBinding {}
